ff2249cb036dd8ba848491ceb662d4af680a3670
VDA-001 <vdavishu@gmail.com>
2021-02-23 11:47:14 +0530

    .

--- CLanguage/ClgPrgrms/program_2.c
+++ CLanguage/ClgPrgrms/program_2.c
@@ -4,30 +4,26 @@
 char str[100],pat[50],rep[50],ans[100];
 int i,j,c,m,k,flag=0;
 void stringmatch(int a)
-{	i=m=c=j=0;
-	while(str[c]!='\0')
-	{
-		if(str[m]==pat[i])
-		{
+{	
+	i=m=c=j=0;
+	while(str[c]!='\0'){
+		if(str[m]==pat[i]){
 			i++;m++;
-			if(i>=a-1)
-			//if(pat[i]=='\0' )
-			{
-				printf("\b");
-				flag=1;
-				for(k=0;rep[k]!='\0';k++,j++)
-				ans[j] =rep[k];
+			if(pat[i]=='\0'){
+				flag = 1;
+				for(k=0;rep[k]!='\0';k++,j++){
+					ans[j] = rep[k];
+				}
 				i=0;
-				c=m;			
+				c=m;
 			}
 		}
-		else
-		{
-			ans[j]=str[c];
-			j++;
+		else{
+			ans[j] = str[c];
 			c++;
-			m=c;
+			j++;
 			i=0;
+			m=c;
 		}
 	}
 	ans[j]='\0';
@@ -36,15 +32,15 @@ void stringmatch(int a)
 void main()
 {
 	printf("\nEnter a main strign\n");
-	fgets(str,100,stdin);
+	gets(str);
 	printf("\nEnter a pattern string\n");
-	fgets(pat,50,stdin);
+	gets(pat);
 	printf("\nEnter a replace string\n");
-	fgets(rep,50,stdin);
+	gets(rep);
 	int a=strlen(pat);
 	stringmatch(a);
 	if(flag==1)
-	printf("\nThe resultant string is : %s",ans);
+	printf("\nThe resultant string is : %s\n\n",ans);
 	else
 	printf("\nPattern string not found\n");
 }
