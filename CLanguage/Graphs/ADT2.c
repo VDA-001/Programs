92be22ba7dc0e1414d62365015d1863175bba65a
VDA-001 <vdavishu@gmail.com>
2021-04-26 17:08:07 +0530

    .

--- CLanguage/Graphs/ADT2.c
+++ CLanguage/Graphs/ADT2.c
@@ -2,6 +2,7 @@
 
 #include<stdio.h>
 #include<stdlib.h>
+#include<assert.h>
 
 struct ListNode{
     int vertex;
@@ -52,5 +53,50 @@ int InsertEdge(struct graph* G,const struct edge E){
 }
 
 int removeEdge(struct graph* G, const struct edge E){
-    
+    int x,y,v;
+    v= G->V;
+    x = E.source;
+    y = E.destination;
+    if(0>x|| x>v || 0>y || y>v){
+        return 0;
+    }
+    struct ListNode* prev = NULL,* ptr = G->adjList[x];
+    while(ptr!=NULL){
+        if(ptr->vertex==y){
+            if(prev==NULL){
+                G->adjList[x] = ptr->next;
+                free(ptr);
+            }else{
+                prev->next = ptr->next;
+                free(ptr);
+            }
+            return 1;
+        }
+        prev = ptr;
+        ptr = ptr->next;
+    }
 }
+
+struct graph* createGraph(const int vertices){
+    assert(vertices>=0);
+    int i,j;
+    struct graph* G = (struct graph*)malloc(sizeof(struct graph));
+    G->V = vertices;
+    G->E = 0;
+    for(int i=0;i<vertices;i++){
+        G->adjList[i] = (struct ListNode*)malloc(sizeof(struct ListNode));
+        assert(G->adjList[i]!=NULL);
+        G->adjList[i]->vertex = i;
+        G->adjList[i]->next = NULL;
+    }
+    return G;
+}
+
+struct edge* newEdge(int x, int y){
+    struct edge* E = (struct edge*)malloc(sizeof(struct edge));
+    assert(E!=NULL);
+    E->source = x;
+    E->destination = y;
+    return E;
+}
+
