635942a438d815b793fdd32addc12157ec5275d3
VDA-001 <vdavishu@gmail.com>
2021-05-09 19:37:52 +0530

    .

--- C++/Problems/tictactoe.c
+++ C++/Problems/tictactoe.c
@@ -1,38 +1,41 @@
 #include<stdio.h>
 #include<stdlib.h>
-
 int main(){
-    int r[3],c[3],d1,d2,t;
-    int a[3][3]={0,0,0,0,0,0,0,0,0};
+    int r[4],c[4],d1,d2,t;
+    char a[3][3];
     scanf("%d",&t);
     for(int h=0;h<t;h++){
-        int o=0,temp=0;
-        d1=1;
+        int o=0;
+        d1=1,d2=1;
+        r={1,1,1};
+        c={1,1,1};
         for(int i=0;i<3;i++){
-            r[i]=1;
-            c[i]=1;
             for(int j=0;j<3;j++){
-                scanf("%d",&temp);
-                a[i][j]=temp;
+                scanf("%c",&a[i][j]);
                 if(a[i][j]=='_'){
                     o+=1;
-                    continue;
+                    r[i]=0;
+                    c[i]=0;
                 }
                 if(i==0 && j==0){
                     continue;
                 }
                 else{
-                    if(r[i]==1 && a[i][j-1]!=a[i][j]){
+                    if(r[i]!=0 && (j!=0 && a[i][j]!=a[i][j-1])){
                         r[i]=0;
                     }
-                    if(i!=0){
-                        if(c[j]==1 && a[i][j]!=a[i-1][j]){
-                            c[j]=0;
-                        }
+                    if(c[j]!=0 && (i!=0 && a[i][j]!=a[i-1][j])){
+                        c[j]=0;
+                        cout<<c[j]<<endl;
                     }
                     if(i==j){
-                        if(d1==1 && a[i][j]!=a[i-1][j-1]){
+                        if(d1==1 && (a[i][j]!=a[i-1][j-1])){
                             d1=0;
+                        }
+                    }
+                    if((i==j && i==1) || (i==2 && j==0)){
+                        if(d2 && (a[i][j]!=a[i-1][j+1])){
+                            d2=0;
                         }
                     }
                 }
@@ -50,16 +53,17 @@ int main(){
         if(d1==1){
             count++;
         }
+        if(d2 == 1){
+            count++;
+        }
         if(count>1){
             printf("%d\n",3);
-        }else if(count==1 && o==0){
-            printf("%d\n",1);
-        }
-        else{
+        }else if((count==0 && o!=0)){
             printf("%d\n",2);
         }
-
+        else{
+            printf("%d\n",1);
+        }
     }
-
     return 0;
 }
--- C++/Problems/tictactoe.cpp
+++ C++/Problems/tictactoe.cpp
@@ -2,37 +2,40 @@
 using namespace std;
 
 int main(){
-    int r[3],c[3],d1,d2,t;
-    int a[3][3]={0,0,0,0,0,0,0,0,0};
+    int d1,d2,t;
+    char a[3][3];
     cin>>t;
     for(int h=0;h<t;h++){
-        int o=0,temp=0;
-        d1=1;
+        int o=0;
+        d1=1,d2=1;
+        int r[3]={1,1,1};
+        int c[3]={1,1,1};
         for(int i=0;i<3;i++){
-            r[i]=1;
-            c[i]=1;
             for(int j=0;j<3;j++){
-                cin>>temp;
-                a[i][j]=temp;
+                cin>>a[i][j];
                 if(a[i][j]=='_'){
                     o+=1;
-                    continue;
+                    r[i]=0;
+                    c[i]=0;
                 }
                 if(i==0 && j==0){
                     continue;
                 }
                 else{
-                    if(r[i]==1 && a[i][j-1]!=a[i][j]){
+                    if(r[i]!=0 && (j!=0 && a[i][j]!=a[i][j-1])){
                         r[i]=0;
                     }
-                    if(i!=0){
-                        if(c[j]==1 && a[i][j]!=a[i-1][j]){
-                            c[j]=0;
-                        }
+                    if(c[j]!=0 && (i!=0 && a[i][j]!=a[i-1][j])){
+                        c[j]=0;
                     }
                     if(i==j){
-                        if(d1==1 && a[i][j]!=a[i-1][j-1]){
+                        if(d1==1 && (a[i][j]!=a[i-1][j-1])){
                             d1=0;
+                        }
+                    }
+                    if((i==j && i==1) || (i==2 && j==0)){
+                        if(d2 && (a[i][j]!=a[i-1][j+1])){
+                            d2=0;
                         }
                     }
                 }
@@ -42,6 +45,7 @@ int main(){
         for(int i=0;i<3;i++){
             if(r[i]==1){
                 count++;
+                
             }
             if(c[i]==1){
                 count++;
@@ -50,16 +54,17 @@ int main(){
         if(d1==1){
             count++;
         }
+        if(d2 == 1){
+            count++;
+        }
         if(count>1){
             cout<<3<<endl;
-        }else if(count==1 && o==0){
-            cout<<1<<endl;
-        }
-        else{
+        }else if((count==0 && o!=0)){
             cout<<2<<endl;
         }
-
+        else{
+            cout<<1<<endl;
+        }
     }
-
     return 0;
 }
