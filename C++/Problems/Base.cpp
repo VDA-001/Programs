2873c12eda317c8fc404eb7465dfe81ad46b8f31
VDA-001 <vdavishu@gmail.com>
2021-05-18 11:09:00 +0530

    .

--- C++/Problems/Base.cpp
+++ C++/Problems/Base.cpp
@@ -1,5 +1,4 @@
 #include<iostream>
-#include<cmath>
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -8,31 +7,13 @@ using namespace std;
 #define ull unsigned long long
 #define ld long double
 
-const int mod = 1e9+7;
+const int mod = 1e+7;
 
 #define input(arr,n) for(ull i=0;i<n;i++){cin>>arr[i];}
 #define output(arr,n) for(ull i=0;i<n;i++){cout<<arr[i];}
 #define fastio ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
 
-const int  MX =1e5 +5;
-vector<int> arr(MX);
-
-void power(){
-    arr[1]=1;
-    for(int i=2;i<MX;i++){
-        arr[i]=(arr[i-1]*2 )%mod;
-    }
-}
-
 int main(){
-    fastio;
-    ll t,n;
-    cin>>t;
-    power();
-    while(t>0){
-        cin>>n;
-        cout<<arr[n]<<endl;
-        t--;
-    }
+    
     return 0;
 }
